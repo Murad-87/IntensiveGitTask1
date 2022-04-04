bbac094960f97d0bbc66318829b5d01c82533c9b
unknown <89380041414@gmail.com>
2022-04-04 14:10:19 +0300

    Добавлена вторая кнопка и реализовано нажатие с изменением TextView

--- app/src/main/java/com/example/intensivetest/MainActivity.kt
+++ app/src/main/java/com/example/intensivetest/MainActivity.kt
@@ -12,8 +12,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val text : TextView = findViewById(R.id.tv_counter)
-
         val btn : Button = findViewById(R.id.btn_click)
+        val btnBack : Button = findViewById(R.id.btn_back)
+
+        btnBack.setOnClickListener {
+            text.text = "Hello World"
+        }
+
         btn.setOnClickListener {
 
             text.text = "Hello Aston"
