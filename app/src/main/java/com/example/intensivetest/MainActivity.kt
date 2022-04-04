4c7c5b62dbe6d4dbebc55b565023a4d7004fc4fa
unknown <89380041414@gmail.com>
2022-04-04 14:03:07 +0300

    Реализовано нажатие на кнопку и изменение TextView

--- app/src/main/java/com/example/intensivetest/MainActivity.kt
+++ app/src/main/java/com/example/intensivetest/MainActivity.kt
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val text : TextView = findViewById(R.id.tv_counter)
+
         val btn : Button = findViewById(R.id.btn_click)
+        btn.setOnClickListener {
+
+            text.text = "Hello Aston"
+        }
     }
 }
