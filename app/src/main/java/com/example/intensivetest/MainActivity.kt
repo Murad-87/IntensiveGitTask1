f66ad8c7a16bc2c6eca7360e0b54b19a598772c9
unknown <89380041414@gmail.com>
2022-04-04 13:51:50 +0300

    Добавил кнопку

--- app/src/main/java/com/example/intensivetest/MainActivity.kt
+++ app/src/main/java/com/example/intensivetest/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.intensivetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn : Button = findViewById(R.id.btn_click)
     }
 }
